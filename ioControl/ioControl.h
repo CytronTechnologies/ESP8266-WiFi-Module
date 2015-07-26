ead2de708fcb96ab75bb0ac21640a419fab2aeb4
bengchet <bengchet@cytron.com.my>
2015-07-27 00:27:10 +0800

    Updated ESP8266
    Added some programs and example sketch

---
+++ ioControl/ioControl.h
@@ -0,0 +1,19 @@
+#ifndef header_h
+#define header_h
+
+String file1=
+"<html>\r\n"
+"<head>\r\n"
+"	<title>ESP8266 GPIO Control</title>\r\n"
+"</head>\r\n"
+"<body>\r\n"
+"<h2>ESP8266 GPIO Control</h2>\r\n"
+"\r\n"
+"<form action = \"/gpio0\" method=\"GET\">GPIO 0: <input type=\"submit\" name=\"on\" value=\"ON\"/>&nbsp;<input type=\"submit\" name=\"off\" value=\"OFF\"/>&nbsp;</form>\r\n"
+"<form action = \"/gpio1\" method=\"GET\">GPIO 1: <input type=\"submit\" name=\"on\" value=\"ON\"/>&nbsp;<input type=\"submit\" name=\"off\" value=\"OFF\"/>&nbsp;</form>\r\n"
+"<form action = \"/gpio2\" method=\"GET\">GPIO 2: <input type=\"submit\" name=\"on\" value=\"ON\"/>&nbsp;<input type=\"submit\" name=\"off\" value=\"OFF\"/>&nbsp;</form>\r\n"
+"\r\n"
+"</body>\r\n"
+"</html>\r\n";
+
+#endif
